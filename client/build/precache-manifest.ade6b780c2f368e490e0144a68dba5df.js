1571551fc6f51fc48e74093e6ec011bfe1e8b6de
Youssef Benadda <ybenadda@e1r4p15.1337.ma>
2020-02-20 07:16:18 +0100

    add

---
+++ client/build/precache-manifest.ade6b780c2f368e490e0144a68dba5df.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4929bd6cdf5ba1299e50ab416556ab59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b96554dfd08fb78cd4a",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "801793c4bff4605631a6",
+    "url": "/static/css/main.ccf14d4d.chunk.css"
+  },
+  {
+    "revision": "7b96554dfd08fb78cd4a",
+    "url": "/static/js/2.e409de00.chunk.js"
+  },
+  {
+    "revision": "e124124f096e36d350c0353f2c6d8998",
+    "url": "/static/js/2.e409de00.chunk.js.LICENSE"
+  },
+  {
+    "revision": "801793c4bff4605631a6",
+    "url": "/static/js/main.6d252567.chunk.js"
+  },
+  {
+    "revision": "bc791e789ac242bb7bfe",
+    "url": "/static/js/runtime-main.6f1a7f07.js"
+  },
+  {
+    "revision": "be89e1e31c3729a406dfe3d01bf29df6",
+    "url": "/static/media/47.be89e1e3.jpg"
+  },
+  {
+    "revision": "6e55d74b09b9cee4df4e86c66667163e",
+    "url": "/static/media/covers.6e55d74b.jpg"
+  },
+  {
+    "revision": "7bf35e72f50ad30beec100fc25589d07",
+    "url": "/static/media/deadpool_2.7bf35e72.jpeg"
+  },
+  {
+    "revision": "5b33a53f612d1dc8ddb3d05ab3c6f63f",
+    "url": "/static/media/joker.5b33a53f.jpg"
+  },
+  {
+    "revision": "b624d72caa5942fb9aeb2185f262e3c0",
+    "url": "/static/media/lucifer.b624d72c.jpg"
+  },
+  {
+    "revision": "a2189e4f04780930cf700402dc5b58fe",
+    "url": "/static/media/snowden.a2189e4f.jpeg"
+  },
+  {
+    "revision": "4594356dbbec7560de3dce02b666beba",
+    "url": "/static/media/starwars.4594356d.jpg"
+  }
+]);
